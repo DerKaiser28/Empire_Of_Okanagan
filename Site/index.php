6cd5eb15eb55bd9ca6a5039f6b997d4642e86524
DerKaiser28 <73674206+DerKaiser28@users.noreply.github.com>
2020-10-30 05:21:17 +0530

    Create index.php

---
+++ Site/index.php
@@ -0,0 +1,100 @@
+<html>
+        <head>
+                <title>Empire Of Okanagan - Main Menu</title>
+        </head>
+
+        <body style="background-color: white;margin:0;">
+
+            <style> 
+
+                  @media screen and (min-device-width : 768px) and (max-device-width : 1024px) and (orientation : portrait){
+                    ul{
+                      width:20%
+                    }
+                  
+
+                    div{
+                      margin-left:20%;
+                      padding:1px 16px;
+                      height:1000px;
+                    }   
+                  
+                  }
+
+                  ul {
+                    list-style-type: none;
+                    margin: 0;
+                    padding: 0;
+                    width: 15%;
+                    background-color: whitesmoke;
+                    position: fixed;
+                    height: 100%;
+                    overflow: auto;
+                  }
+
+                  
+                  div{
+                      margin-left:15%;
+                      padding:1px 16px;
+                      height:1000px;
+                    }  
+
+                  li a {
+                    display: block;
+                    color: black;
+                    padding: 8px 16px;
+                    text-decoration: none;
+                  }
+
+                  li a:hover:not(.active) {
+                    background-color:#003366;
+                    color: white;
+                  }
+
+                .newspaper {
+                  column-count: 3;
+                  column-gap: 40px;
+                  column-rule-style: solid;
+                  column-rule-width: 1px;
+                  column-rule-color: #003366;
+                  font-size: 120%;
+                }
+
+                h7{
+                  font-size: 120%;
+                  font-family: Arial, Helvetica, sans-serif;
+
+                }
+
+                
+
+            </style>
+            
+            <ul>
+              <img src="UBC.png" width=200 height=60/>
+              <br>
+              <h7><li><a href="index.php">Home</a></li>
+              <li><a href="site.php">Voting</a></li>
+              <li><a href="">Polling</a></li>
+              <li><a href="">Schedule</a></li>
+              <li><a href="#info">Information</a></li></h7>
+             
+            </ul>
+
+            <div>
+
+            <h1 style="color:black;font-family: 'Times New Roman', Times, serif;text-align:left"><img src="maple.png"; width="50" height="50" style="vertical-align:-40%" > Empire Of Okanagan</h1>
+            <hr>
+            
+            </div>
+
+            <br>
+
+            
+
+            
+
+
+
+        </body>
+</html>
